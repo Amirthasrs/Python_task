861a2874f399c9a4f4a1537cb4756842bbd98241
Amirthasrs <amirthaifs@gmail.com>
2025-11-03 16:46:01 +0530

    task

---
+++ Day6 task.py
@@ -0,0 +1,53 @@
+print("Print numbers from 1 to 20.")
+for i in range(1,21):
+    print(i)
+print()
+print("Print all even numbers from 2 to 50")
+for i in range(2,50,2):
+    print(i)
+    print()
+print("Print all odd number from 1 to 50.")
+for i in range(1,51,2):
+    print(i)
+    print()
+print("Print the square of number 1 to 15.")
+for i in range(1,16):
+    print(i*i)
+    print()
+print("Print the cube numbers from 1 to 10.")
+for i in range(1,11):
+    print(i*i*i)
+    print()
+print("Print the numbers from 10 down to 1 in reverse order")
+for i in range(10,0,-1):
+    print(i)
+    print()
+print("5 th Multiplication table")
+for i in range(1,11):
+    print(f"5X {i} = {5*i}")
+    print()
+print("Print all character of the string one by one")
+string= "Amirtha"
+for i in string:
+    print(i)
+print("Print numbers divisible by 3 between 1 to 30.")
+for i in range(1,31):
+    if (i%3==0):
+        print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
